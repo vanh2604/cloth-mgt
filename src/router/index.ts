150ac4233569b12bdb843ef0a608cc221afa3484
Vu Dinh Viet Anh <vietanhvu@beeketing.net>
2023-05-15 21:32:31 +0700

    fix url

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// @ts-ignore
 import HomeView from '../views/HomeView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +17,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+      // @ts-ignore
       component: () => import('../views/AboutView.vue')
     }
   ]
--- src/uses/useCloth.ts
+++ src/uses/useCloth.ts
@@ -6,13 +6,13 @@ const dataSource = ref([])
 const useCloth =  () => {
         const listStyle = ref([])
         const fetchData = async () => {
-            const response = await fetch('http://ec2-13-215-140-47.ap-southeast-1.compute.amazonaws.com:8080/api/clothes/getAll')
+            const response = await fetch('https://ec2-13-215-140-47.ap-southeast-1.compute.amazonaws.com:8080/api/clothes/getAll')
             const data = await response.json()
             dataSource.value = data
         }
         const handleDelete = async (id : any) => {
             try {
-                await axios.delete(`http://ec2-13-215-140-47.ap-southeast-1.compute.amazonaws.com:8080/api/clothes/delete/${id}`)
+                await axios.delete(`https://ec2-13-215-140-47.ap-southeast-1.compute.amazonaws.com:8080/api/clothes/delete/${id}`)
                 message.success('Data deleted successfully!')
                 await fetchData()
             } catch (error) {
@@ -20,7 +20,7 @@ const useCloth =  () => {
             }
         }
         const getAllStyle = async () => {
-        const {data} = await axios.get("http://ec2-13-215-140-47.ap-southeast-1.compute.amazonaws.com:8080/api/style/getAll")
+        const {data} = await axios.get("https://ec2-13-215-140-47.ap-southeast-1.compute.amazonaws.com:8080/api/style/getAll")
         const optionStyle = data.map((option : any) => {
             return {
                 value : option.styleId,
